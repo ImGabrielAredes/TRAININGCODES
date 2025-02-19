0948b076b40a2865a403fda17482a586e2ba9f8d
gabrielaredes <gabriel.aredes@sga.pucminas.br>
2025-02-19 16:12:53 -0300

    declaracao das variaveis

---
+++ contas.c/.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    //declaração das variáveis
+
+    int ID;
+    char NM;
+
+    printf("Escreva o nome do usuario : ");
+    scanf("%c",&NM);
+
+    printf("Escreva a idade do usuário :");
+    scanf("%d",&ID);
+    
+
+}
