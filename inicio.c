515ef4c9d8a896b6e8de75d8264e173e58da5924
Gabriel Aredes <gabriel.aredes@sga.pucminas.br>
2025-02-19 22:37:26 -0300

    declaracao da idade do user

---
+++ inicio.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    // declaração das variaveis de idade
+    int ID;
+
+    printf ("Digite a idade da pessoa: ");
+    scanf ("%d", &ID);
+    
+    
+    ])
+}
